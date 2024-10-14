8966de2409c717b20c873235325add49877a8b48
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2024-10-14 18:45:11 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ app.use((req, res) => {
     fetch("https://pvcmapper.my.to?" + req.url.split("?=")[1] ).then(async (result) => {
         res.setHeader("Content-Type", result.headers.get("Content-Type"))
         console.log(result.headers.get("Content-Type"))
-        if(result.headers.get("Content-Type") == "text/html" || result.headers.get("Content-Type") == "text/css" || result.headers.get("Content-Type") == "application/javascript") {
+        if(result.headers.get("Content-Type").contains("text/html") || result.headers.get("Content-Type").contains("text/css") || result.headers.get("Content-Type").contains("application/javascript")) {
             console.log("Acceptable to text replace")
             let r = await result.text()
             r.replace(/"\//gm, "/api?=")
