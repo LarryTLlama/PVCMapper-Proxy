898b06b42d712b598247123362a4d23ec6a2c935
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2024-10-14 18:30:39 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const fetch = require("node-fetch");
 const path = require("path")
 
 app.use((req, res) => {
+    console.log(req.url)
     let url = new URL(req.url)
     fetch("https://pvcmapper.my.to" + url.pathname.replace("/api", "") + url.hash + url.search ).then((result) => {
         res.setHeader("Content-Type", result.headers.get("Content-Type"))
