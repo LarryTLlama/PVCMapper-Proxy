04db174a6e569ade7ccf13763d42a0f667ce5b44
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2024-10-14 18:41:01 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,9 +4,15 @@ const fetch = require("node-fetch");
 const path = require("path")
 
 app.use((req, res) => {
-    fetch("https://pvcmapper.my.to" + req.url.replace("/api", "") ).then((result) => {
+    fetch("https://pvcmapper.my.to?" + req.url.split("?=")[1] ).then(async (result) => {
         res.setHeader("Content-Type", result.headers.get("Content-Type"))
-        result.body.pipe(res);
+        if(result.headers.get("Content-Type") == "text/html" || result.headers.get("Content-Type") == "text/css" || result.headers.get("Content-Type") == "application/javascript") {
+            let r = await result.text()
+            r.replace(/"\//gm, "/api?=")
+            res.send(r)
+        } else {
+            result.body.pipe(res)
+        }
     });
 })
 
