729bcbc4c6134e2f0e44a7cd5584258daf73ba9c
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2024-10-14 19:14:45 +0100

    Now works 🎉

--- api/index.js
+++ api/index.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
-const path = require("path")
 
 app.use((req, res) => {
-    fetch("https://pvcmapper.my.to?" + req.url.split("?=")[1] ).then(async (result) => {
+    fetch("https://pvcmapper.my.to" + req.url).then(async (result) => {
         res.setHeader("Content-Type", result.headers.get("Content-Type"))
-        console.log(result.headers.get("Content-Type"))
-        if(result.headers.get("Content-Type").contains("text/html") || result.headers.get("Content-Type").contains("text/css") || result.headers.get("Content-Type").contains("application/javascript")) {
+        /*console.log(typeof result.headers.get("Content-Type"))
+        if(result.headers.get("Content-Type").includes("text/html") || result.headers.get("Content-Type").includes("text/css") || result.headers.get("Content-Type").includes("application/javascript")) {
             console.log("Acceptable to text replace")
             let r = await result.text()
-            r.replace(/"\//gm, "/api?=")
-            res.send(r)
+            res.send(r.replace(/".\//gm, "\"/api?url="))
         } else {
             console.log("Standard - Do not replace")
-            result.body.pipe(res)
-        }
+            
+        }*/
+        result.body.pipe(res)
     });
 })
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
