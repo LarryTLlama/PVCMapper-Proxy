d8245044d44d52837b7ac95b15443e8cc5279766
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2024-10-14 18:27:20 +0100

    Starting Commit

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+const path = require("path")
+
+app.use((req, res) => {
+    let url = new URL(req.url)
+    fetch("https://pvcmapper.my.to" + url.pathname + url.hash + url.search ).then((result) => {
+        res.setHeader("Content-Type", result.headers.get("Content-Type"))
+        result.body.pipe(res);
+    });
+})
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
